6bb5a3b883e574b42f0216275492c0a57cf3ebc6
Dev-Thilina <work.thilinatharuphati@gmail.com>
2026-01-23 00:32:15 +0530

    add_database

---
+++ database/paws_and_hearts.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 22, 2026 at 08:01 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `paws_and_hearts`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pets`
+--
+
+CREATE TABLE `pets` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `image_path` varchar(255) NOT NULL,
+  `pet_specie` varchar(50) NOT NULL,
+  `breed` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `pets`
+--
+
+INSERT INTO `pets` (`id`, `name`, `image_path`, `pet_specie`, `breed`) VALUES
+(1, 'Slotty', '/assets/uploads/pets/50d9d593-c777-483d-b98b-53aa3dd610a2.jpg', 'Sloth', 'Blue Sloth'),
+(2, 'Lassy', '/assets/uploads/pets/dog.jpg', 'Dog', 'Labradoor'),
+(3, 'Kitty', '/assets/uploads/pets/cat.jpg', 'Cat', 'Orangey');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `pets`
+--
+ALTER TABLE `pets`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `pets`
+--
+ALTER TABLE `pets`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
