7d94d4687bcbb06cc93ba128bf121d863997d548
Dev-Thilina <work.thilinatharuphati@gmail.com>
2026-01-29 01:47:28 +0530

    updated_database

--- database/paws_and_hearts.sql
+++ database/paws_and_hearts.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 22, 2026 at 08:01 PM
+-- Generation Time: Jan 28, 2026 at 09:13 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -24,6 +24,52 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `adopter`
+--
+
+CREATE TABLE `adopter` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `gender` varchar(20) NOT NULL,
+  `contact` varchar(100) NOT NULL,
+  `street_address` varchar(255) NOT NULL,
+  `city` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `adopter`
+--
+
+INSERT INTO `adopter` (`id`, `name`, `gender`, `contact`, `street_address`, `city`) VALUES
+(1, 'New Adoptor', 'Male', 'newadp@gmail.com', 'harath', 'panadura'),
+(3, 'Testing Girl adopter', 'Female', 'test-girl21@gmail.com', 'meesa-yick', 'new-york');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `caretaker`
+--
+
+CREATE TABLE `caretaker` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `gender` varchar(25) NOT NULL,
+  `contact` varchar(100) NOT NULL,
+  `dob` date NOT NULL,
+  `experience` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `caretaker`
+--
+
+INSERT INTO `caretaker` (`id`, `name`, `gender`, `contact`, `dob`, `experience`) VALUES
+(1, 'Thilina Tharuphati', 'Male', 'tannercase@gmail.com', '2026-01-15', 2),
+(3, 'Testing Girl User', 'Female', 'test-girl@gmail.com', '2003-04-02', 3);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `pets`
 --
 
@@ -32,21 +78,56 @@ CREATE TABLE `pets` (
   `name` varchar(50) NOT NULL,
   `image_path` varchar(255) NOT NULL,
   `pet_specie` varchar(50) NOT NULL,
-  `breed` varchar(50) NOT NULL
+  `breed` varchar(50) NOT NULL,
+  `description` varchar(5000) NOT NULL,
+  `status` int(11) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `pets`
 --
 
-INSERT INTO `pets` (`id`, `name`, `image_path`, `pet_specie`, `breed`) VALUES
-(1, 'Slotty', '/assets/uploads/pets/50d9d593-c777-483d-b98b-53aa3dd610a2.jpg', 'Sloth', 'Blue Sloth'),
-(2, 'Lassy', '/assets/uploads/pets/dog.jpg', 'Dog', 'Labradoor'),
-(3, 'Kitty', '/assets/uploads/pets/cat.jpg', 'Cat', 'Orangey');
+INSERT INTO `pets` (`id`, `name`, `image_path`, `pet_specie`, `breed`, `description`, `status`) VALUES
+(8, 'Kitty', 'assets/uploads/pets/cat.jpg', 'Cat', 'Orangey', '', 0),
+(9, 'ajcv ', 'assets/uploads/pets/art-attack-Qqn9VFkdV6E-unsplash.png', 'snf dvb ', 'smdfv bh', '', 0),
+(10, 'Diggy', 'assets/uploads/pets/dog.jpg', 'Dog', 'Whitemile Dog', 'cool and playful dog that you can play with every day', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `email` varchar(150) NOT NULL,
+  `password` varchar(225) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `email`, `password`) VALUES
+(1, 'super-admin', 'sadmin@gmail.com', '$2y$10$JJBU6nVDCgrM4y341p/EUOWsKzRfaGlDeUGLSKA.uPpMTvQjXSi0a'),
+(2, 'admin', 'admin@gmail.com', '$2y$10$2xdUqeAbWfW3NzCa4lRyCutGVZIQQYEZfdcnrcnnWdK7RRt5BW266');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `adopter`
+--
+ALTER TABLE `adopter`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `caretaker`
+--
+ALTER TABLE `caretaker`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `pets`
@@ -55,14 +136,38 @@ ALTER TABLE `pets`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `adopter`
+--
+ALTER TABLE `adopter`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `caretaker`
+--
+ALTER TABLE `caretaker`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `pets`
 --
 ALTER TABLE `pets`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
